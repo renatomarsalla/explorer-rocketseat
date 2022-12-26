b75312866c8c533e8303f639d376bfeda5bdb16f
Renato Marsalla Toscano <renatomarsalla@gmail.com>
2022-12-26 18:26:56 -0300

    iniciando trabalho com imagens

--- Desafio_Final/foodExplorer/apiFoodExplorer/src/routes/dishesAdmin.routes.js
+++ Desafio_Final/foodExplorer/apiFoodExplorer/src/routes/dishesAdmin.routes.js
@@ -1,13 +1,21 @@
 const { Router } = require('express');
+const multer = require('multer');
+const uploadConfig = require('../configs/upload')
 
 const { DishesAdminController } = require('../controllers/DishesAdminController');
 
 const dishesRoutes = Router();
+
+const upload = multer(uploadConfig.MULTER);
 
 const dishesAdminController = new DishesAdminController();
 
 dishesRoutes.post('/', dishesAdminController.createDishes);
 dishesRoutes.put('/:id', dishesAdminController.updateDish);
 dishesRoutes.delete('/:id', dishesAdminController.deleteDish);
+dishesRoutes.patch('/avatar', upload.single("avatar"), (request, response) => {
+  console.log(request.file.filename);
+  response.json();
+})
 
 module.exports = { dishesRoutes };
