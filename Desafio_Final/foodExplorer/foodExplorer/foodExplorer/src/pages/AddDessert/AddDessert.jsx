b8ed16370fee48c9603af31014e0304858e9f8f0
Renato Marsalla Toscano <renatomarsalla@gmail.com>
2022-12-28 18:08:27 -0300

    criado novas pages para drinks e desserts

--- Desafio_Final/foodExplorer/foodExplorer/foodExplorer/src/pages/AddDessert/AddDessert.jsx
+++ Desafio_Final/foodExplorer/foodExplorer/foodExplorer/src/pages/AddDessert/AddDessert.jsx
@@ -9,7 +9,7 @@ import { MdKeyboardArrowLeft } from 'react-icons/md';
 
 import { HiOutlineShoppingBag } from 'react-icons/hi';
 
-function AddDish() {
+function AddDessert() {
   return (
     <Container>
       <header>
@@ -47,7 +47,7 @@ function AddDish() {
         <main>
           <div className="return">
             <Button text="Voltar" icon={MdKeyboardArrowLeft} />
-            <h2>Adicionar prato</h2>
+            <h2>Adicionar sobremesa</h2>
           </div>
 
           <div className="imageAndNameDish">
@@ -94,4 +94,4 @@ function AddDish() {
   );
 }
 
-export { AddDish };
+export { AddDessert };
--- Desafio_Final/foodExplorer/foodExplorer/foodExplorer/src/routes/app.routes.jsx
+++ Desafio_Final/foodExplorer/foodExplorer/foodExplorer/src/routes/app.routes.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { AddDish } from '../pages/AddDish/AddDish';
+import { AddDessert } from '../pages/AddDessert/AddDessert';
+import { AddDrink } from '../pages/AddDrink/AddDrink';
 import { Details } from '../pages/Details/Details';
 import { EditDish } from '../pages/EditDish/EditDish';
 import { Home } from '../pages/Home/Home';
@@ -11,6 +13,8 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/addDish" element={<AddDish />} />
+      <Route path="/addDessert" element={<AddDessert />} />
+      <Route path="/addDrink" element={<AddDrink />} />
       <Route path="/details/:id" element={<Details />} />
       <Route path="/editDish/:id" element={<EditDish />} />
       <Route path="/" element={<Home />} />
